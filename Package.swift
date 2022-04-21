8baf6fe78bf2b995d8df8a711dd2d3ed75f82e02
Stanislav Kutil <skutil@acrobits.cz>
2022-04-21 11:14:22 +0200

    fixed the URL

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "SipisProvider",
-            url: "https://dist.acrobits.net/saas/sipis-provider-swift-package/release/SipisProvider.xcframework-175238.zip",
+            url: "https://dist.acrobits.net/sipisprovider-swift-package/release/SipisProvider.xcframework-175238.zip",
             checksum: "9112875897e61981f198675865af056a35f16195a84dbc923e496284b2e95be0"),
     ]
 )
